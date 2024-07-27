f390e97c2cb414b98436bbdcafc02a6c14fec1e9
Menino123 <a.rpjalves@colegioatlantico.pt>
2024-07-27 13:21:57 +0100

    Update hello_code.c

--- week 02/Course C Codes/hello_code.c
+++ week 02/Course C Codes/hello_code.c
@@ -6,5 +6,5 @@ int main(void)
 
     string first = get_string("What's your first name? \n");
     string last = get_string("What's your last name? \n");
-printf("hello, %s %s!\n", first, last);
+    printf("hello, %s %s!\n", first, last);
 }
